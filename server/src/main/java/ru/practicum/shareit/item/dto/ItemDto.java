1939fc41af42daa2de602011ef05deb56e58c99e
Viktor Wagner <vvagner.viktor@yandex.ru>
2024-07-28 13:15:43 +0200

    Add ControllersTest

--- server/src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
+++ server/src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
@@ -3,9 +3,11 @@ package ru.practicum.shareit.item.dto;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@Builder
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
